6fff02144c8a6222ccb80742a217f2ce42bffac2
mateusz.rosner <rosner.mateusz@gmail.com>
2021-12-09 09:34:56 +0100

    d

--- logger.py
+++ logger.py
@@ -3,9 +3,9 @@ import os
 
 from datetime import datetime
 
-LABELS = {"Date" : " ", "Time" : " ", "PowTotal" : 0.0, "WodaPitna1" : 0, "WodaPitna2" : 0, "WodaBrudna" : 0,  "Szambo" : 0, "Paliwo" : 0, 
-          "TempSalon" : 0.0, "TempKuchnia" : 0.0, "TempTaras" : 0.0, "PressSalon" : 0.0, "PressTaras" : 0.0,
-          "HumidSalon" : 0.0, "HumidKuchnia" : 0.0, "HumidTaras" : 0.0}
+LABELS = {"Date" : " ", "Time" : " ", "PowTotal" : "0.0", "WodaPitna1" : "0", "WodaPitna2" : "0", "WodaBrudna" : "0",  "Szambo" : "0", "Paliwo" : "0", 
+          "TempSalon" : "0.0", "TempKuchnia" : "0.0", "TempTaras" : "0.0", "PressSalon" : "0.0", "PressTaras" : "0.0",
+          "HumidSalon" : "0.0", "HumidKuchnia" : "0.0", "HumidTaras" : "0.0"}
 
 class Logger():
     def __init__(self):
@@ -44,21 +44,21 @@ class Logger():
 
         _labels["Date"] = date
         _labels["Time"] = time
-        _labels["PowTotal"] = resources.total_curr
-        _labels["WodaPitna1"] = resources.liquids[0]
-        _labels["WodaPitna2"] = resources.liquids[1]
-        _labels["WodaBrudna"] = resources.liquids[2]
-        _labels["Szambo"] = resources.liquids[3]
-        _labels["Paliwo"] = resources.liquids[4]
-        _labels["TempSalon"] = resources.temperature[0]
-        _labels["TempKuchnia"] = resources.temperature[1]
-        _labels["TempTaras"] = resources.temperature[2]
-        _labels["PressSalon"] = resources.pressure[0]
-        _labels["PressKuchnia"] = resources.pressure[1]
-        _labels["PressTaras"] = resources.pressure[2]
-        _labels["HumidSalon"] = resources.humidity[0]
-        _labels["HumidKuchnia"] = resources.humidity[1]
-        _labels["HumidTaras"] = resources.humidity[2]
+        _labels["PowTotal"] = str(resources.total_curr)
+        _labels["WodaPitna1"] = str(resources.liquids[0])
+        _labels["WodaPitna2"] = str(resources.liquids[1])
+        _labels["WodaBrudna"] = str(resources.liquids[2])
+        _labels["Szambo"] = str(resources.liquids[3])
+        _labels["Paliwo"] = str(resources.liquids[4])
+        _labels["TempSalon"] = str(resources.temperature[0])
+        _labels["TempKuchnia"] = str(resources.temperature[1])
+        _labels["TempTaras"] = str(resources.temperature[2])
+        _labels["PressSalon"] = str(resources.pressure[0])
+        _labels["PressKuchnia"] = str(resources.pressure[1])
+        _labels["PressTaras"] = str(resources.pressure[2])
+        _labels["HumidSalon"] = str(resources.humidity[0])
+        _labels["HumidKuchnia"] = str(resources.humidity[1])
+        _labels["HumidTaras"] = str(resources.humidity[2])
 
         with open(os.path.join(path, date), 'a') as f:
             f.write(",".join(list(LABELS.values())) + '\n')
