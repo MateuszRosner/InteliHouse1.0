cc6f70e2c23b66f10e7cf47be85044a2c7023ab1
mateusz.rosner <rosner.mateusz@gmail.com>
2021-12-30 14:13:14 +0100

    rew

--- InteliHouse.py
+++ InteliHouse.py
@@ -239,8 +239,8 @@ class MyWindow(Ui_MainWindow):
 
         #if (self.tabWidget.currentIndex() == 0):
         # create and draw power consumption chart
-        self.chartPower.axisX(self.powerData)
-        self.chartPower.axisY(self.powerData)
+        #self.chartPower.axisX(self.powerData)
+        #self.chartPower.axisY(self.powerData)
         self.chartPower.createDefaultAxes()
         self.widget_2.setChart(self.chartPower)
 
