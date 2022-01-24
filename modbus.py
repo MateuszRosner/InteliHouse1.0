a204bcb034cec1e1cc151e3f52633b5c5b7a2869
mateusz.rosner <rosner.mateusz@gmail.com>
2022-01-24 13:06:33 +0100

    f

--- modbus.py
+++ modbus.py
@@ -49,12 +49,12 @@ class Modbus():
         
         if self.ser.isOpen():
             frame.calcCRC()
-            GPIO.output(TXDEN_2, GPIO.LOW)     # transmitter
+            #GPIO.output(TXDEN_2, GPIO.LOW)     # transmitter
             time.sleep(0.0005)
             frame = bytearray(frame)
             self.ser.write(frame)
             time.sleep(0.0005)
-            GPIO.output(TXDEN_2, GPIO.HIGH)    # reciver
+            #GPIO.output(TXDEN_2, GPIO.HIGH)    # reciver
 
     def read_data(self):
         if self.ser.isOpen() == True:
