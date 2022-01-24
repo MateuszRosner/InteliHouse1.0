b1f9c5178fc258047ad0fe6760195809bd46554d
mateusz.rosner <rosner.mateusz@gmail.com>
2022-01-24 13:53:09 +0100

    we

--- modbus.py
+++ modbus.py
@@ -16,7 +16,7 @@ BAUD_38400 = 38400
 
     
 class Modbus():
-    def __init__(self, baudrate=38400, dev="/dev/ttySC1", crcControl=True, dataLen=8):
+    def __init__(self, baudrate=9600, dev="/dev/ttySC1", crcControl=True, dataLen=8):
          # --------------- config file reading    ---------------
         config = configparser.ConfigParser()
         config.read('config.ini')
