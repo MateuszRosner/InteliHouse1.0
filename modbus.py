021e99c258554fcabb32be0937e2aa0fe5e85033
mateusz.rosner <rosner.mateusz@gmail.com>
2022-01-26 13:40:49 +0100

    eqw

--- modbus.py
+++ modbus.py
@@ -102,7 +102,7 @@ class Modbus():
         frame.data[0] = 0x00
         frame.data[1] = 0x01
         frame.data[2] = 0x00
-        frame.data[3] = 0x02
+        frame.data[3] = 0x01
         
         self.send_frame(frame)
 
