65c838744626bde8525bcde06f684d38ecf6ef29
mateusz.rosner <rosner.mateusz@gmail.com>
2022-01-26 13:42:00 +0100

    tst

--- modbus.py
+++ modbus.py
@@ -100,9 +100,9 @@ class Modbus():
         frame.address = 0x01
         frame.command = 0x03
         frame.data[0] = 0x00
-        frame.data[1] = 0x01
+        frame.data[1] = 0x02
         frame.data[2] = 0x00
-        frame.data[3] = 0x01
+        frame.data[3] = 0x02
         
         self.send_frame(frame)
 
