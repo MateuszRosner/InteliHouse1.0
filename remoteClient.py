3e2607cfe5b946427af228b18d72cee44321fea2
mateusz.rosner <rosner.mateusz@gmail.com>
2022-04-05 10:30:36 +0200

    client data send recive

--- remoteClient.py
+++ remoteClient.py
@@ -1,10 +1,17 @@
 import requests
+from datetime import datetime
 from requests.exceptions import HTTPError
 
-for url in ['http://localhost:8080/']:
+for url in ['http://192.168.0.29:6013/addData']:
     try:
         #response = requests.get(url)
-        response = requests.post(url, json={'current_user_url': 'https://api.github.com/user'})
+        date_time = datetime.now()
+        date_time_parse = date_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        response = requests.post(url, json={'valdate' : date_time_parse, 'powtotal': 123, 'powout1' : 1, "powout2" : 1, "powout3" : 1, "powout4" : 1,	"powout5" : 1, "powout6" : 1,
+                                        	"powout7" : 1, "powout8" : 1, "powout9" : 1, "powout10" : 1, "wodapitna1" : 1, "wodapitna2" : 1, "wodabrudna" : 1, "szambo" : 1, "paliwo" : 1,
+                                            "temp1" : 1, "temp2" : 1, "temp3" : 1, "press1" : 1, "press2" : 1, "press3" : 1, "humid1" : 1, "humid2" : 1, "humid3" : 2, "uq_house_id" : "domek1", "ac_state" : 1,
+                                            "ac_temp" : 21.5})
 
         response.raise_for_status()
     except HTTPError as http_err:
